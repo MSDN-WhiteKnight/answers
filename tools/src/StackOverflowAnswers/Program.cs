ca441b1955f5db3e9a71e2bf668fb053e50c20a1
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-09-11 20:45:32 +0500

    Preserve title when saving answers

--- tools/src/StackOverflowAnswers/Program.cs
+++ tools/src/StackOverflowAnswers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
 using RuSoLib;
@@ -7,6 +8,108 @@ namespace StackOverflowAnswers
 {
     class Program
     {
+        public static void SaveUserAnswers(string site, int userid)
+        {
+            string datadir = "..\\..\\..\\..\\data\\" + site + "\\";
+            string postsdir = Path.Combine(datadir, "posts\\");
+            string path;
+
+            if (!Directory.Exists(postsdir)) Directory.CreateDirectory(postsdir);
+
+            SeApiClient client = new SeApiClient(Archive.APIURL, site);
+            Dictionary<int, object> answers = client.LoadUserAnswers(userid);
+
+            Console.WriteLine("Saving {0} answers...", answers.Count);
+
+            foreach (int key in answers.Keys)
+            {
+                path = Path.Combine(postsdir, "A" + key.ToString() + ".md");
+                string title = "Answer " + key.ToString();
+
+                //if answer is already saved, use title from existing file
+
+                try
+                {
+                    if (File.Exists(path))
+                    {
+                        TextReader rd = new StreamReader(path, Encoding.UTF8);
+                        using (rd)
+                        {
+                            AnswerMarkdown prev = AnswerMarkdown.FromMarkdown(site, rd);
+
+                            if (!String.IsNullOrEmpty(prev.Title)) title = prev.Title;
+                        }
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("Error when trying to read existing answer data");
+                    Console.WriteLine(ex.ToString());
+                }
+
+                TextWriter wr = new StreamWriter(path, false, Encoding.UTF8);
+                using (wr)
+                {
+                    AnswerMarkdown post = AnswerMarkdown.FromJsonData(site, answers[key]);
+                    post.Title = title;
+                    post.ToMarkdown(wr);
+                }
+            }
+        }
+
+        public static void SaveSingleAnswer(string site, int id)
+        {
+            string datadir = "..\\..\\..\\..\\data\\" + site + "\\";
+            string postsdir = Path.Combine(datadir, "posts\\");
+            string path;
+
+            if (!Directory.Exists(postsdir)) Directory.CreateDirectory(postsdir);
+            Console.WriteLine("Saving single answer {0} from {1}...", id, site);
+
+            SeApiClient client = new SeApiClient(Archive.APIURL, site);
+            string a = client.LoadSingleAnswer(id);
+
+            if (a == null)
+            {
+                throw new Exception("Failed to load answer " + id.ToString() + " from " + site);
+            }
+
+            path = Path.Combine(postsdir, "A" + id.ToString() + ".md");
+            string title = "Answer " + id.ToString();
+
+            //if answer is already saved, use title from existing file
+
+            try
+            {
+                if (File.Exists(path))
+                {
+                    TextReader rd = new StreamReader(path, Encoding.UTF8);
+                    using (rd)
+                    {
+                        AnswerMarkdown prev = AnswerMarkdown.FromMarkdown(site, rd);
+
+                        if (!String.IsNullOrEmpty(prev.Title)) title = prev.Title;
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Error when trying to read existing answer data");
+                Console.WriteLine(ex.ToString());
+            }
+
+            TextWriter wr = new StreamWriter(path, false, Encoding.UTF8);
+            using (wr)
+            {
+                dynamic data = JSON.Parse(a);
+                AnswerMarkdown post = AnswerMarkdown.FromJsonData(site, data);
+                post.Title = title;
+                post.ToMarkdown(wr);
+            }
+
+            Console.WriteLine("Success");
+        }
+
         [STAThread]
         static void Main(string[] args)
         {
@@ -32,12 +135,12 @@ namespace StackOverflowAnswers
             }
             else if (args.Length >= 3 && args[0] == "savea")
             {
-                Archive.SaveSingleAnswer(args[1], Convert.ToInt32(args[2]));
+                SaveSingleAnswer(args[1], Convert.ToInt32(args[2]));
                 Console.WriteLine("Done");
             }
             else if (args.Length >= 3 && args[0] == "saveu")
             {
-                Archive.SaveUserAnswers(args[1], Convert.ToInt32(args[2]));
+                SaveUserAnswers(args[1], Convert.ToInt32(args[2]));
                 Console.WriteLine("Done");
             }
             else if (args.Length >= 3 && args[0] == "sync")
